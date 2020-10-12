48a3dceb7af5561e9d6df1b57c8251169368e001
Harun <67702854+hrndgc@users.noreply.github.com>
2020-10-12 22:48:59 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const firstSentence = document.querySelector("#firstSentence");
+const button_1 = document.querySelector("#button_1");
+const reverseSentence = document.querySelector("#reverseSentence");
+
+button_1.addEventListener("click", reverseWord);
+firstSentence.addEventListener("keyup", enter);
+
+function reverseWord() {
+  if (firstSentence.value === "") {
+    alert("Please enter a sentence");
+  }
+  const reverse = firstSentence.value.split(" ").reverse().join(" ");
+  reverseSentence.innerHTML = reverse;
+  firstSentence.focus();
+  firstSentence.value = "";
+};
+
+function enter(event) {
+  if (event.keyCode===13){
+    reverseWord();
+  }
+};
